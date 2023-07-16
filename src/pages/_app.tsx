c100ff62cbab97ee3c813743330bcccfbe209ab9
Raushan Kumar <raushan.development@gmail.com>
2023-07-16 19:07:06 +0530

    google login feature added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleOAuthProvider clientId="294256846544-a9a43cbmqee525e1o65aqagmfj37jl9u.apps.googleusercontent.com">
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { TbSocial } from 'react-icons/tb'
-import { BiHomeCircle, BiSearchAlt2, BiSolidEnvelope, BiDotsHorizontalRounded } from 'react-icons/bi'
-import { IoMdNotifications } from 'react-icons/io'
-import { BsBookmarksFill } from 'react-icons/bs'
-import { CgProfile } from 'react-icons/cg'
-import FeedCard from '@/components/FeedCard'
-const inter = Inter({ subsets: ['latin'] })
+import React from "react";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { GoogleLogin } from "@react-oauth/google";
+import { TbSocial } from "react-icons/tb";
+import {
+  BiHomeCircle,
+  BiSearchAlt2,
+  BiSolidEnvelope,
+  BiDotsHorizontalRounded,
+} from "react-icons/bi";
+import { IoMdNotifications } from "react-icons/io";
+import { BsBookmarksFill } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import FeedCard from "@/components/FeedCard";
+const inter = Inter({ subsets: ["latin"] });
 
 interface AppSideBar {
   title: string;
@@ -17,79 +23,84 @@ interface AppSideBar {
 const AppSideBarItems: AppSideBar[] = [
   {
     title: "Home",
-    icon: <BiHomeCircle />
+    icon: <BiHomeCircle />,
   },
   {
     title: "Explore",
-    icon: <BiSearchAlt2 />
+    icon: <BiSearchAlt2 />,
   },
   {
     title: "Notifications",
-    icon: <IoMdNotifications />
-  }, 
+    icon: <IoMdNotifications />,
+  },
   {
     title: "Messages",
-    icon: <BiSolidEnvelope />
-  }, 
+    icon: <BiSolidEnvelope />,
+  },
   {
     title: "Bookmarks",
-    icon: <BsBookmarksFill />
-  }, 
+    icon: <BsBookmarksFill />,
+  },
   {
     title: "Profile",
-    icon: <CgProfile />
+    icon: <CgProfile />,
   },
   {
     title: "More",
-    icon: <BiDotsHorizontalRounded />
+    icon: <BiDotsHorizontalRounded />,
   },
-]
-
+];
 
 export default function Home() {
   return (
     <div className={inter.className}>
-      <div className='grid grid-cols-12 h-screen w-screen px-32'>
-        <div className='col-span-3 '>
-          <div className='ml-[-5px] text-3xl text-blue-600 flex flex-row items-center gap-4 mt-1 p-2 hover:bg-gray-100 h-fit w-fit rounded-full transition-all'>
+      <div className="grid grid-cols-12 h-screen w-screen px-32">
+        <div className="col-span-3 ">
+          <div className="ml-[-5px] text-3xl text-blue-600 flex flex-row items-center gap-4 mt-1 p-2 hover:bg-gray-100 h-fit w-fit rounded-full transition-all">
             <TbSocial />
           </div>
           <div>
-            <ul className='mt-4 flex flex-col '>
+            <ul className="mt-4 flex flex-col ">
               {AppSideBarItems.map((item) => (
-                <li className='flex flex-row gap-3 items-center mb-8 hover:bg-gray-200 h-fit w-fit px-2 py-2 rounded-full transition-all' key={item.title}>
-                  <span className='text-xl'>
-                    {item.icon}
-                  </span>
-                  <span className='text-xl '>
-                    {item.title}
-                  </span>
+                <li
+                  className="flex flex-row gap-3 items-center mb-4 hover:bg-gray-200 h-fit w-fit px-2 py-2 rounded-full transition-all"
+                  key={item.title}
+                >
+                  <span className="text-xl">{item.icon}</span>
+                  <span className="text-xl ">{item.title}</span>
                 </li>
               ))}
             </ul>
           </div>
-          <div className='mt-1 pr-6'>
-            <button className='bg-blue-500 text-xl p-3 text-white w-full rounded-full'>Post</button>
+          <div className="mt-1 pr-6">
+            <button className="bg-blue-500 text-xl p-3 text-white w-full rounded-full">
+              Post
+            </button>
           </div>
         </div>
-        <div className='col-span-5 border-l-2 border-r-2 h-screen overflow-scroll no-scrollbar border-gray-200 dark:border-gray-400'>
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
-         <FeedCard />
+        <div className="col-span-5 border-l-2 border-r-2 h-screen overflow-scroll no-scrollbar border-gray-200 dark:border-gray-400">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
         </div>
-        <div className='col-span-4 '></div>
+        <div className="col-span-4 ">
+          <div className=" px-4 py-8 space-y-2">
+            <h1 className="font-semibold">New to Socialo?</h1>
+            <GoogleLogin onSuccess={(cred) => console.log(cred)} />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
