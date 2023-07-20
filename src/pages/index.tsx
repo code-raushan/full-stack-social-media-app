2759ce9da974eb4ca3e321b8fb56ab7c2978b778
Raushan Kumar <raushan.development@gmail.com>
2023-07-20 12:45:49 +0530

    added user badge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import {toast} from 'react-hot-toast'
+import { toast } from 'react-hot-toast'
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import { TbSocial } from "react-icons/tb";
 import {
@@ -57,18 +57,18 @@ const AppSideBarItems: AppSideBar[] = [
 ];
 
 export default function Home() {
-  const {user} = useCurrentUser();
+  const { user } = useCurrentUser();
   const queryClient = useQueryClient()
-  const handleGoogleLogin =  useCallback(async(cred:CredentialResponse)=>{
+  const handleGoogleLogin = useCallback(async (cred: CredentialResponse) => {
     const googleToken = cred.credential;
     console.log(googleToken)
-    if(!googleToken) throw new Error('token does not exist');
-    
+    if (!googleToken) throw new Error('token does not exist');
+
     // sending the graphql request to the backend server, with the token that we have received from google oauth2
-    const {verifyGoogleToken} = await graphqlClient.request(VerifyGoogleToken, {token: googleToken});
+    const { verifyGoogleToken } = await graphqlClient.request(VerifyGoogleToken, { token: googleToken });
     console.log(verifyGoogleToken);
     toast.success("User Signed In");
-    if(verifyGoogleToken){
+    if (verifyGoogleToken) {
       window.localStorage.setItem("__app_token", verifyGoogleToken);
     }
     await queryClient.invalidateQueries(["CURRENT_USER"]);
@@ -76,7 +76,7 @@ export default function Home() {
   return (
     <div className={inter.className}>
       <div className="grid grid-cols-12 h-screen w-screen px-32">
-        <div className="col-span-3 ">
+        <div className="relative col-span-3">
           <div className="ml-[-5px] text-3xl text-blue-600 flex flex-row items-center gap-4 mt-1 p-2 hover:bg-gray-100 h-fit w-fit rounded-full transition-all">
             <TbSocial />
           </div>
@@ -93,11 +93,22 @@ export default function Home() {
               ))}
             </ul>
           </div>
-          <div className="mt-1 pr-6">
-            <button className="bg-blue-500 text-xl p-3 text-white w-full rounded-full">
+          <div className="mt-1 pr-16">
+            <button className="bg-blue-500 text-xl px-1 py-2 text-white w-3/4 rounded-full">
               Post
             </button>
           </div>
+          {user &&
+            <div className="absolute bottom-10 flex gap-3 items-center hover:bg-gray-300 px-4 py-3 rounded-lg">
+              <div>
+                {user && user.profileImg && <Image src={user.profileImg} height={50} width={50} className="rounded-full" alt="user-img" />}
+              </div>
+              <div>
+                {user && <h1 className="font-semibold">{user.firstName}</h1>}
+                {user && <h1 className="font-semibold">{user.lastName}</h1>}
+              </div>
+            </div>
+          }
         </div>
         <div className="col-span-5 border-l-2 border-r-2 h-screen overflow-scroll no-scrollbar border-gray-200 dark:border-gray-400">
           <FeedCard />
