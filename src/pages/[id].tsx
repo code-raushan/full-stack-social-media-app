9d84c7c7a622adbbc19f1637da50cf297dfbfb2c
Raushan Kumar <raushan.development@gmail.com>
2023-07-27 00:00:38 +0530

    profile page rendering done

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -14,7 +14,7 @@ interface ServerProps {
     user?: User
 }
 
-const UserProfilePage: NextPage<ServerProps> = ()=>{
+const UserProfilePage: NextPage<ServerProps> = (props)=>{
     const router = useRouter();
     const {user} = useCurrentUser();
     return (
@@ -25,19 +25,19 @@ const UserProfilePage: NextPage<ServerProps> = ()=>{
                         <BsArrowLeftShort className="text-4xl" />
                         <div>
                             <h1 className="text-2xl font-bold">
-                                {user?.firstName} {user?.lastName}
+                                {props.user?.firstName} {props.user?.lastName}
                             </h1>
                             <h1 className="text-md font-bold text-slate-500">
-                                {user?.posts?.length}
+                                {props.user?.posts?.length}
                             </h1>
                         </div>
                     </nav>
                     <div className="p-4 border">
-                        {user?.profileImg && <Image src={user?.profileImg} alt="user-image" height={150} width={150} className="rounded-full"/>}
-                        <h1 className="text-2xl font-bold mt-5">{user?.firstName} {user?.lastName}</h1>
+                        {props.user?.profileImg && <Image src={props.user?.profileImg} alt="user-image" height={150} width={150} className="rounded-full"/>}
+                        <h1 className="text-2xl font-bold mt-5">{props.user?.firstName} {props.user?.lastName}</h1>
                     </div>
                     <div>
-                        {user?.posts?.map((post)=> <FeedCard data={post as Post} key={post?.id}/>)}
+                        {props.user?.posts?.map((post)=> <FeedCard data={post as Post} key={post?.id}/>)}
                     </div>
                 </div>
             </AppLayout>
@@ -48,14 +48,14 @@ const UserProfilePage: NextPage<ServerProps> = ()=>{
 export const getServerSideProps: GetServerSideProps = async(context)=>{
     const id = context.query.id as string | undefined;
     if(!id) return {notFound: true, props: {user: undefined}}
-    const userInfo = await graphqlClient.request(getUserByIdQuery, {id});
+    const user = await graphqlClient.request(getUserByIdQuery, {id});
 
-    if(!userInfo?.getUserById) return {notFound: true}
+    if(!user?.getUserById) return {notFound: true}
 
     console.log(id);
     return {
         props:{
-            userInfo: userInfo.getUserById as User
+            user: user.getUserById as User
         }
     }
 }
